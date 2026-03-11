98a33e276cf5690c3422eead82334e23cb0a8500
aryanrao <60213378+aryanrao@users.noreply.github.com>
2026-03-11 08:20:17 +0530

    Add files via upload

---
+++ Experiment5/Experiment5.sql
@@ -0,0 +1,53 @@
+-- 
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT
+);
+
+
+
+
+-- 
+INSERT INTO employee VALUES (1, 'Rja', 'HR', 25001);
+INSERT INTO employee VALUES (2, 'Soumya', 'Finance', 32000);
+INSERT INTO employee VALUES (3, 'Amay', 'IT', 45015);
+INSERT INTO employee VALUES (4, 'Nehul', 'Marketing', 28000);
+INSERT INTO employee VALUES (5, 'Bhagvat', 'Sales', 39011);
+
+
+
+-- 
+SELECT id, name, department, salary
+FROM employee;
+
+
+
+--
+SELECT 
+    name,
+    salary,
+    CASE 
+        WHEN salary % 2 = 0 THEN 'Even Salary'
+        ELSE 'Odd Salary'
+    END AS salary_type
+FROM employee;
+
+
+
+--
+SELECT 
+    name,
+    salary
+FROM employee
+WHERE salary % 2 <> 0;
+
+
+
+-- 
+SELECT 
+    name,
+    salary
+FROM employee
+WHERE salary % 2 = 0;
